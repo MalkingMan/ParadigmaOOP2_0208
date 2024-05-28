67d966c45fd8110152d4da7a80279deecd7c7da4
MalkingMan <arrayalkhozini@gmail.com>
2024-05-28 15:59:50 +0700

    commit 15

--- Abstraksi.cpp
+++ Abstraksi.cpp
@@ -17,6 +17,15 @@ public:
     //menampilkan nilai
     void display()
     {
-        
+        cout << "x =" << x << endl;
+        cout << "y =" << y << endl;
     }
+};
+
+int main() {
+	AbstraksiKlas ak;
+	ak.setXY("Yogyakarta", "Kampus");
+	ak.display();
+
+	return 0;
 }
